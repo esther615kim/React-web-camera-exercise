851a73f5888f4384290fb069b9af88cc0a5b8214
esther615kim <75744588+esther615kim@users.noreply.github.com>
2021-09-07 18:45:28 +0900

    server.js: comment added

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const express = require('express');
+const app = express();
+const server = require('http').Server(app); //서버 생성
+const io = require('socket.io')(server);//서버 socket.io에 넘김
+const { v4: uuidV4 } = require('uuid');//아이디
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs') //
+app.use(express.static('public')) // set up static folders,퍼블릭으로
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
+app.get('/', (req, res) => { //create brand new room &
+  res.redirect(`/${uuidV4()}`) //랜덤 uuid로 생성된 방에 dynamic 리다이렉
 })
 
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
-})
+}) //get a room
 
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
